5457ecb70c574a182cada112118aa6a1059b669f
guatra <gennadiy.korshunov@gmail.com>
2017-07-13 22:51:14 +0700

    frontend add

--- frontend/views/movies/view.php
+++ frontend/views/movies/view.php
@@ -12,12 +12,6 @@ use common\widgets\Alert;
 use yii\helpers\FileHelper;
 
 ?>
-    <h1>movies/view</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
 
 
 <?= VideoJsWidget::widget([
